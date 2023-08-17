a1879df028b7e24a92d7b2288c6a5afdc684c192
Gautammak <gautammakavana2001@gmail.com>
2023-08-17 14:31:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 //weather  project-2
 
-import React from 'react'
-import Temp from './component/weather/temp.js'
-
-const App = () => {
-  return (
-    <>
-      <Temp />
-    </>
-  )
-}
- 
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import React from 'react'
-// import Todo from './component/todoreact/todo'
+// import Temp from './component/weather/temp.js'
 
 // const App = () => {
 //   return (
 //     <>
-//       <Todo />
+//       <Temp />
 //     </>
 //   )
 // }
-
+ 
 // export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react'
+import Todo from './component/todoreact/todo'
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  )
+}
+
+export default App
